fbcc470b173a8aef1d63e3700e4829a26d9d125f
= <mayorlen.ortega@gmail.com>
2022-07-16 02:02:22 -0400

    Ruteo y navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import {Link} from "react-router-dom"
+import Logo from '../img/logotetarte negro.png'
+
+const Navbar = () => {
+return (
+    <>
+    <div className="line"></div>
+    <nav className="navbar navbar-dark bg-dark">
+         <Link to="/" className="links-nav">
+           
+        </Link>
+        <Link to="/" className="links-nav">
+            Inicio
+        </Link>
+        <Link to="/blog" className="links-nav">
+            Blog
+        </Link>
+        <Link to="/somos" className="links-nav">
+            Quiénes Somos
+        </Link>
+        <Link to="/paises" className="links-nav">
+            Países
+        </Link>
+        <Link to="/media" className="links-nav">
+            Media
+        </Link>
+        <Link to="/Contacto" className="links-nav">
+            Contacto
+        </Link>
+        <div></div>
+    </nav>
+    <img src={Logo} className="logo"></img>
+    </>
+)
+
+};
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Blog from './views/Blog';
+import Somos from './views/Somos';
+import Media from './views/Media';
+import Paises from './views/Paises';
+import Contacto from './views/Contacto'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/views/Blog" element={<Blog />} />
+    <Route path="/views/Somos" element={<Somos />} />
+    <Route path="/Paises" element={<Paises />} />
+    <Route path="/views/Media" element={<Media />} />
+    <Route path="/views/Constacto" element={<Contacto />} />
+     </Routes>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
