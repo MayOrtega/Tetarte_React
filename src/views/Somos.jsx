ff7f23f5c19e9d0bcaef7cd8688c66b28bf0bfa4
= <mayorlen.ortega@gmail.com>
2022-07-19 19:04:18 -0400

    Delete apli maps

---
+++ src/views/Somos.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+
+
+const Somos = () => {
+  return (
+    <>
+    <div className="quienes-somos">
+      <div>
+        <Navbar />
+      </div>
+      <div className="content-quienes-somos">
+      <section className="section1-quienes-somos">
+        <h1 className="title-quienes-somos">¿Quiénes somos?</h1>
+        <p>
+          Tetarte es un proyecto fotográfico pro lactancia materna cuyos
+          objetivos son promover la lactancia materna, desmitificarla y
+          visualizarla como acto humano y natural. Proyecto de ONG Grial, marca
+          registrada y protegido por ley de derechos de autor.
+        </p>
+      </section>
+      <section className="section2-quienes-somos">
+      
+    
+    </section>
+     
+            </div>
+      </div>
+    </>
+  );
+};
+
+export default Somos;
