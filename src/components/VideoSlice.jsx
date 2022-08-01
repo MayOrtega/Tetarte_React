53e77bc4f51bb2fcff1a86af95f9ce16b699b261
= <mayorlen.ortega@gmail.com>
2022-08-01 00:30:28 -0400

    install reactPlayer

--- src/components/VideoSlice.jsx
+++ src/components/VideoSlice.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import video from '../img/videoTetarte.mp4'
+import ReactPlayer from 'react-player'
 
 
 const VideoSlice = () => {
   return (
     <div className="content-video-slice">
-      <h1 className="title-videos">Videos TetArte</h1>
+      <h1 className="title-videos">Testimonio</h1>
+      <section className="section-video">
+      <ReactPlayer url={video} width='70%' controls />
+      </section>
       
     </div>
   );
