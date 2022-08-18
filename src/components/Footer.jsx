887227f0c9560b458df4e0f71047e35854fa6e6b
MayorlenO <mayorlen.ortega@gmail.com>
2022-08-18 02:38:12 -0400

    update  footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import loguito from '../img/loguito-footer.png'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <h1 className='title-footer'>© 2022 Sitio Web desarrollado por May Ortega</h1>
+      <img src={loguito} alt='' className='loguito'></img>
     </div>
   )
 }
